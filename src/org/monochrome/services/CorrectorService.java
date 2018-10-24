1a0945ee0e84d63a5c3de58f8c358f84bc2fcca1
monochromePinguin <origil@orangefr>
2018-10-24 13:52:58 +0200

    new: classes under service/ are fleshed

--- src/org/monochrome/services/CorrectorService.java
+++ src/org/monochrome/services/CorrectorService.java
@@ -1,4 +1,7 @@
 package org.monochrome.services;
 
 public class CorrectorService {
+    //minimum percentage of rightly-answered questions for a quizz to be considered "passed"
+    public static final int winMinPercentage = 75;
+
 }
--- src/org/monochrome/services/SingleLogger.java
+++ src/org/monochrome/services/SingleLogger.java
@@ -1,4 +1,7 @@
 package org.monochrome.services;
 
+import java.util.logging.Logger;
+
 public class SingleLogger {
+    public static final Logger logger = Logger.getLogger("common");
 }
